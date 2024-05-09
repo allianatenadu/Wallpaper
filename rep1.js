4b2e211f5a288062d6d2c478cdeb44227606d71d
Aliana Tenadu <alianatenadu@gmail.com>
2024-05-09 14:40:04 +0000

    first collabration

---
+++ rep1.js
@@ -0,0 +1,9 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the icon is clicked */
+function toggleMenu() {
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+        x.className += " responsive";
+    } else {
+        x.className = "topnav";
+    }
+}
